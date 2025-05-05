ed86530426f88b1312611c7609db862f19db9f11
Shanna <shanna@jonesbunch.com>
2025-05-05 15:50:53 +0200

    Updated Results and created Meaning components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
   function search(event) {
     event.preventDefault();
@@ -20,10 +23,12 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
   return (
+    // First Results is the component name, second is the property name, and third is the data we are requesting, property value
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
