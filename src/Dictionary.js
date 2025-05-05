a791da1c9f88de89e4a4e52921e2347e47d56a80
Shanna <shanna@jonesbunch.com>
2025-05-05 14:17:10 +0200

    updated keywords

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 
-export default function Dictionary(props) {
-  const [keyword, setKeyword] = useState("");
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
     console.log(response.data);
@@ -11,8 +11,8 @@ export default function Dictionary(props) {
   function search(event) {
     event.preventDefault();
 
-    const apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
-    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    let apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
