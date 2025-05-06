3215421d0d92459eade627d0ffb7ae026538d17e
Shanna <shanna@jonesbunch.com>
2025-05-06 11:08:49 +0200

    Created synonyms module

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h5>
+        {" "}
+        <strong>Part of Speech: </strong>
+        {props.meaning.partOfSpeech}
+      </h5>
 
       <p>
-        {props.meaning.definition}
+        <strong>Definition: </strong> {props.meaning.definition}
         <br></br>
+        <strong>Example(s): </strong>
         <em>{props.meaning.example}</em>
+        <br></br>
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
